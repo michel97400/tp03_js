132a584275d5f1e5e01dbf9ff92c5357e69afdb4
michel97400 <michel.payet974@live.fr>
2025-02-15 19:30:15 +0100

    ajout

--- main.js
+++ main.js
@@ -7,5 +7,36 @@ let person = {
 
 };
 
+// Resquest name
+person.name = prompt("Enter your name: ");
+
+// Verification
+while (!person.name) {
+    person.name = prompt("Enter your valid name: ");
+}
+
+// Request fullname
+person.fullname = prompt("Enter your full name: ");
+
+// Verification
+while (!person.fullname) {
+    person.fullname = prompt("Enter your  valid fullname: ");
+}
+
+// Request email
+person.email = prompt("Enter a email");
+
+// Verification
+while (!person.email) {
+    person.email = prompt("Enter your valid email: ");
+}
+
+// Request age
+person.age = Number(prompt("Enter your age: "));
+
+// Verification
+while (isNaN(person.age) || !person.age) {
+    person.age = Number(prompt("Enter your valid age: "));
+}
 
 
